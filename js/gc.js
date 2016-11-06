a923ed374b8a6444762335ee2e98b3ec95d735b3
orzechowskid <orzechod@gmail.com>
2016-11-05 20:38:51 -0400

    responsive!

--- js/gc.js
+++ js/gc.js
@@ -5,7 +5,6 @@
 	var href = event.currentTarget.getAttribute('href').slice(1);
 
 	event.preventDefault();
-	console.log(href);
 	document.querySelector('a[name="' + href + '"]').scrollIntoView({
 	    behavior: 'smooth'
 	});
